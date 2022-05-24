626caf27548b2519c62b50cc0e1c73c21064d094
Elias K. Salem <EliasKSalemTech@gmail.com>
2022-05-24 12:33:08 -0700

    Add files via upload

---
+++ Dentist Webpage/script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#menu-btn');
+const navbar = document.querySelector('.header .nav');
+const header = document.querySelector('.header');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if (window.scrollY > 0) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+}
